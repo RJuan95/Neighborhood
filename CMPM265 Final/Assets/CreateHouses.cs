9a6de4c78767b6e862cad45f2abd73d06c567b91
Juan <jurosale@ucsc.edu>
2018-06-13 19:25:07 -0700

    generates new materials and locations

--- CMPM265 Final/Assets/CreateHouses.cs
+++ CMPM265 Final/Assets/CreateHouses.cs
@@ -5,75 +5,133 @@ using AX;
 
 public class CreateHouses : MonoBehaviour
 {
+    public Vector2 SizeRange;
     public AXModel model;
-    //public AXParameter sizeX, sizeY, sizeZ, posX, posY, posZ;
-    float randX, dist;
-    //RuntimeStuff rts;
-    //GameObject[] models;
-    //int copies;
-    //bool check;
+    public List<AXGameObject> border;
+    public AXGameObject entrance;
+    public AXGameObject backEntrance;
+    public AXGameObject carEntrance;
+    public List<AXGameObject> LongWindows;
+    public List<AXGameObject> ShortWindows;
+    public AXGameObject house;
+    public AXGameObject houseGar;
+    public AXGameObject top;
+    public AXGameObject topGar;
+    public AXGameObject field;
+    public AXGameObject land;
+    public List<Material> Fence, Door, Backdoor, Garage, BigWindow, LittleWindow, Walls, Roof, Grass, Landscape;
+
+    float randX, posX, distX, distZ;
 
     // Use this for initialization
     void Start()
     {
-        //rts = GameObject.Find("Main Camera").GetComponent<RuntimeStuff>();
-       // models = GameObject.FindGameObjectsWithTag("Models");
-
         if (model != null)
         {
-            //sizeX = model.getParameter("Full House_Scale_X");
-            //sizeY = model.getParameter("Full House_Scale_Y");
-            //sizeZ = model.getParameter("Full House_Scale_Z");
-            //posX = model.getParameter("Full House_Trans_X");
-            //posY = model.getParameter("Full House_Trans_Y");
-            //posZ = model.getParameter("Full House_Trans_Z");
+            randX = model.transform.localScale.x;
+            distX = model.transform.position.x;
+            posX = distX;
+            distZ = model.transform.position.z;
+
         }
-        dist = 0;
-        randX = 1f; 
-        //check = false;
     }
 
     private void Update()
     {
         //clones a new house with different dimensions 
+        if (Input.GetKeyDown(KeyCode.Space)|| Input.GetKeyDown(KeyCode.Backspace))
+        {
+            GenerateMaterials();
+            GenerateHouse();
+        }
+    }
+
+    void GenerateHouse()
+    {
+        //ensures houses are created in a horizontal line
+        float oldRandX = randX;
+        randX = Random.Range(SizeRange.x, SizeRange.y);
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            //int copies = models.Length - 1;
-            //Debug.Log(copies);
-            float oldRandX = randX;
-            randX = Random.Range(.5f, 1.5f);
-            dist += (10.2f * randX) + (10.2f * oldRandX);  
-            AXModel newModel = Instantiate(model, new Vector3(dist , model.transform.position.y, model.transform.position.z), Quaternion.identity);
-            newModel.GetComponent<CreateHouses>().enabled = false;
-            //changes the width of the landscape
-            newModel.getParameter("Full House_Scale_X").initiateRipple_setFloatValueFromGUIChange(randX);
-            //changes the height of the landscape
-            newModel.getParameter("Full House_Scale_Y").initiateRipple_setFloatValueFromGUIChange(Random.Range(.5f, 1.5f));
-            //changes the length of the landscape
-            newModel.getParameter("Full House_Scale_Z").initiateRipple_setFloatValueFromGUIChange(Random.Range(.5f, 1.5f));
-
-            model = newModel;
-            model.build();
-            Debug.Log(newModel.transform.position);
-            //if (copies > 0)
-            //{
-            //    check = true;
-            //}
+            distX += (10.2f * randX) + (10.2f * oldRandX);
         }
 
-        //if (check)
-        //{
-        //    models = GameObject.FindGameObjectsWithTag("Models");
-        //    Debug.Log("._>. " + gameObject.name);
-        //    if (models.Length - copies > 1)
-        //    {
-        //        for (int i = models.Length - 2; i >= models.Length - (copies + 1); i--)
-        //        {
-        //            Destroy(models[i]);
-        //        }
-        //        check = false;
-        //    }
-        //}
+        if (Input.GetKeyDown(KeyCode.Backspace))
+        {
+            distX = posX;
+            distZ += (27.5f * SizeRange.y * 2f);
+        }
+
+        //Creates a new house
+        AXModel newModel = Instantiate(model, new Vector3(distX, model.transform.position.y, distZ), Quaternion.identity);
+
+        //changes the width of the landscape
+        newModel.getParameter("Full House_Scale_X").initiateRipple_setFloatValueFromGUIChange(randX);
+
+        //changes the height of the landscape
+        newModel.getParameter("Full House_Scale_Y").initiateRipple_setFloatValueFromGUIChange(Random.Range(SizeRange.x, SizeRange.y));
+
+        //changes the length of the landscape
+        newModel.getParameter("Full House_Scale_Z").initiateRipple_setFloatValueFromGUIChange(Random.Range(SizeRange.x, SizeRange.y));
+
+        newModel.build();
+    }
+
+    //changes the materials for each new house
+    void GenerateMaterials()
+    {
+        //changes the fence to a new random material
+        int index = Random.Range(0, Fence.Count);
+        for (int i = 0; i < border.Count; i++)
+        {
+            border[i].parametricObject.axMat.mat = Fence[index];
+        }
+
+        //changes the door to a new random material
+        int index1 = Random.Range(0, Door.Count);
+        entrance.parametricObject.axMat.mat = Door[index1];
+
+        
+        //changes the backdoor to a new random material
+        int index2 = Random.Range(0, Backdoor.Count);
+        backEntrance.parametricObject.axMat.mat = Backdoor[index2];
+
+        //changes the garage to a new random material
+        int index3 = Random.Range(0, Garage.Count);
+        carEntrance.parametricObject.axMat.mat = Garage[index3];
+
+        //changes the big windows to a new random material
+        int index4 = Random.Range(0, BigWindow.Count);
+        for (int i = 0; i < BigWindow.Count; i++)
+        {
+            LongWindows[i].parametricObject.axMat.mat = BigWindow[index4];
+        }
+
+        //changes the small windows to a new random material
+        int index5 = Random.Range(0, LittleWindow.Count);
+        for (int i = 0; i < LittleWindow.Count; i++)
+        {
+            ShortWindows[i].parametricObject.axMat.mat = LittleWindow[index5];
+        }
+
+        //changes the house to a new random material
+        int index6 = Random.Range(0, Walls.Count);
+        house.parametricObject.axMat.mat = Walls[index6];
+        houseGar.parametricObject.axMat.mat = Walls[index6];
+
+        //changes the roof to a new random material
+        int index7 = Random.Range(0, Roof.Count);
+        top.parametricObject.axMat.mat = Roof[index7];
+        topGar.parametricObject.axMat.mat = Roof[index7];
+
+        //changes the grass to a new random material
+        int index8 = Random.Range(0, Grass.Count);
+        field.parametricObject.axMat.mat = Grass[index8];
+
+        //changes the landscape to a new random material
+        int index9 = Random.Range(0, Landscape.Count);
+        land.parametricObject.axMat.mat = Landscape[index9];
     }
 }
 
---
+++ CMPM265 Final/Assets/GenerateMaterial.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AX;
+
+public class GenerateMaterial : MonoBehaviour {
+
+    public List<AXGameObject> fence;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
